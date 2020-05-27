2b849c9a7df2356c2381ac7f28ac5ae98a86ac00
NikitaNogikhin <62744354+NikitaNogikhin@users.noreply.github.com>
2020-05-27 19:07:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,36 @@
 int cbinseacrh(int* arr, int size, int value)
 {
-	int result = 0, i = 0, j = size - 1;
-	printf("Enter the desired value: ", value);
-	while (i < j)
+	int result = 0, left = 0, right = size;
+	int mid = (left + right) / 2
+	while (left <= right)
 	{
-		int mid = (i + j) / 2;
-		if (arr[mid] == value) {
-			return result++;
+		if (arr[mid] == value) 
+		{
+			result++;
+			break;
 		}
-		else if (value <= arr[mid]) {
-			j = mid;
+		if (value > arr[mid]) 
+		{
+			left = mid + 1;
+			mid = (left + right) / 2;
 		}
-		else {
-			i = mid + 1;
+		if (value < arr[mid])
+		{
+			right = mid -1;
+			mid = (left + right) / 2;
 		}
+	}
+	int t = 1;
+	while ((mid - t >= 0) && (arr[mid - t] == value))
+	{
+		result++;
+		t++;
+	}
+	t = 1;
+	while ((mid + t >= 0) && (arr[mid + t] == value))
+	{
+		result++;
+		t++;
 	}
 	return result;
 }
